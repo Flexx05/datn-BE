683c8597e2ba827fd5cb5fbab72b4c0ac4a3319c
Trang-gif <trangcthph51731@gmail.com>
2025-05-13 08:00:07 +0700

    feat : chức năng cập nhật

--- src/controllers/brand.controller.js
+++ src/controllers/brand.controller.js
@@ -31,4 +31,18 @@ export const getAllBrands = async (req, res) => {
     } catch (error) {
       return res.status(500).json({ error: error.message });
     }
-  }
+  };
+
+  export const updateBrand = async (req, res) => {
+    try {
+      const { id } = req.params;
+      const { name, slug, logoUrl } = req.body;
+      const brand = await brandModel.findByIdAndUpdate(id, { name, slug, logoUrl }, { new: true });
+      if (!brand) {
+        return res.status(404).json({ error: "Brand not found" });
+      }
+      return res.status(200).json({ message: "Brand updated successfully", brand });
+    } catch (error) {
+      return res.status(500).json({ error: error.message });
+    }
+  };
--- src/routers/brand.router.js
+++ src/routers/brand.router.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
-import {createBrand, getAllBrands, getBrandById} from "../controllers/brand.controller";
+import {createBrand, getAllBrands, getBrandById, updateBrand} from "../controllers/brand.controller";
 
 const router = Router();
 
 router.post("/create", createBrand);
 router.get("/get-all", getAllBrands);
 router.get("/get-by-id/:id", getBrandById);
+router.put("/update/:id", updateBrand);
 
 
 export default router;
