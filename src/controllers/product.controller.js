4e51aa3d2b7f70d57e443b6aef54490c81ed95fb
Manh Linh <mlinhalone@gmail.com>
2025-05-13 19:49:32 +0700

    feat: Thêm chức năng xóa sản phẩm

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -44,3 +44,18 @@ export const getProductBySlug = async (req, res) => {
   }
 };
 
+export const deleteProduct = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await productModel.findByIdAndUpdate(
+      id,
+      { isActive: false },
+      { new: true }
+    );
+    if (!product)
+      return res.status(404).json({ message: "Sản phẩm khôn tồn tại" });
+    return res.status(200).json(product);
+  } catch (error) {
+    return res.status(400).json({ message: error.message });
+  }
+};
