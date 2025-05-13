232aef007f5d9c76ba0974a28c0fb43ced721d92
Trang-gif <trangcthph51731@gmail.com>
2025-05-13 07:51:53 +0700

    feat: chức năng lấy danh sách

--- src/routers/brand.router.js
+++ src/routers/brand.router.js
@@ -4,5 +4,6 @@ import {createBrand} from "../controllers/brand.controller";
 const router = Router();
 
 router.post("/create", createBrand);
+router.get("/get-all", getAllBrands);
 
 export default router;
