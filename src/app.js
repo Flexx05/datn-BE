85d2aff0413b2cc17a8575fe53a9d583d85f20a6
VikeTus <letuantu0123@gmail.com>
2025-05-14 16:53:41 +0700

    Hoàn thành CRUD Voucher

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
 import authRouter from "./routers/auth.router";
+import voucherRouter from "./routers/voucher.router"
 
 const app = express();
 dotenv.config();
@@ -10,7 +11,8 @@ dotenv.config();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(`mongodb://127.0.0.1:27017/demo`);
+mongoose.connect(`mongodb://127.0.0.1:27017/${process.env.DB_URL}`);
 
 app.use("/api", authRouter);
+app.use("/api",voucherRouter)
 export const viteNodeApp = app;
