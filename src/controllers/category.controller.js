6e87db9864ef4a246183d66877be7f675fe19e8a
Trang-gif <trangcthph51731@gmail.com>
2025-05-15 15:51:12 +0700

    feat: thêm tính năng lấy theo id

--- src/controllers/category.controller.js
+++ src/controllers/category.controller.js
@@ -43,3 +43,22 @@ export const getAllCategories = async (req, res) => {
       return res.status(500).json({ error: error.message });
     }
   };
+
+  export const getCategoryById = async (req, res) => {
+    try {
+      const { id } = req.params;
+      const category = await categoryModel.findById(id)
+        .populate({
+          path: "subCategories",
+          match: { isActive: true },
+          options: { sort: { categorySort: 1 } },
+        });
+      if (!category) {
+        return res.status(404).json({ error: "Category not found" });
+      }
+      return res.status(200).json({ message: "Get category successfully", category });
+    } catch (error) {
+      return res.status(500).json({ error: error.message });
+    }
+  };
+  
--- src/routers/category.router.js
+++ src/routers/category.router.js
@@ -6,6 +6,7 @@ const router = Router();
 
 router.post("/create", createCategory);
 router.get("/get-all", getAllCategories);
+router.get("/get-by-id/:id", getCategoryById);
 
 
 export default router;
