e8266d4fe899abda8907b3a376435a4af67b01f8
VikeTus <letuantu0123@gmail.com>
2025-05-15 23:50:46 +0700

    fix connect db

--- src/app.js
+++ src/app.js
@@ -2,12 +2,13 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
+
 import authRouter from "./routers/auth.router";
 import brandRouter from "./routers/brand.router";
 import attributeRouter from "./routers/attribute.router";
 import productRouter from "./routers/product.router";
-import userRouter from "./routers/admin.user.router";
-import voucherRouter from "./routers/voucher.router"
+// import userRouter from "./routers/admin.user.router";
+import voucherRouter from "./routers/voucher.router";
 
 const app = express();
 
@@ -16,14 +17,18 @@ dotenv.config();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(`mongodb://127.0.0.1:27017/${process.env.DB_URL}`);
+mongoose.connect(
+  `mongodb+srv://${process.env.DB_USERNAME}/${process.env.DB_URL}?retryWrites=true&w=majority&appName=DATN-DB`
+);
+console.log("Connected to MongoDB");
+  
 
-//router
+//route
 app.use("/api", attributeRouter);
 app.use("/api", productRouter);
 app.use("/api", authRouter);
 app.use("/api/brand", brandRouter);
-app.use("/api", userRouter);
-app.use("/api",voucherRouter)
+// app.use("/api", userRouter);
+app.use("/api", voucherRouter);
 
 export const viteNodeApp = app;
