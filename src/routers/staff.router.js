06c3bd92040c760cd21e772fda5caa16ac72af29
Phongln <phonglnph51412@gmail.com>
2025-05-17 16:55:37 +0700

    feat: Cập nhật staff router

--- src/routers/staff.router.js
+++ src/routers/staff.router.js
@@ -1,11 +1,14 @@
-import { Router } from "express";
-import { getAllStaff, updateStaffRole } from "../controllers/staff.controller";
-import { verifyToken } from "../middlewares/checkAuth";
+import express from "express";
+import {
+  getAllStaff,
+  updateStaffRole,
+} from "../controllers/staff.controller.js";
+import { verifyToken, isAdmin } from "../middlewares/checkAuth.js";
 
-const router = Router()
+const router = express.Router();
 
-router.get("/staffs", getAllStaff),
-router.patch("/staffs/:id/role", verifyToken, updateStaffRole)
+router.get("/staffs", verifyToken, isAdmin, getAllStaff);
 
+router.patch("/staffs/:id/role", verifyToken, isAdmin, updateStaffRole);
 
-export default router
+export default router;
