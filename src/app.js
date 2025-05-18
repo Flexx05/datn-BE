7eaf664ff7a67dc58783548fee43e18f5cf8ca05
Trang-gif <trangcthph51731@gmail.com>
2025-05-18 18:08:44 +0700

    fix: connect mongo

--- src/app.js
+++ src/app.js
@@ -3,20 +3,23 @@ import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
 import authRouter from "./routers/auth.router";
-import brandRouter from "./routers/brand.router";
+// import brandRouter from "./routers/brand.router";
 import attributeRouter from "./routers/attribute.router";
-
+import categoryRouter from "./routers/category.router";
 const app = express();
 dotenv.config();
 
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(`mongodb://127.0.0.1:27017/demo`);
+mongoose.connect(
+    `mongodb+srv://root:binova2025@datn-db.nx9ha3d.mongodb.net/${process.env.DB_URL}?authSource=admin&retryWrites=true&w=majority&appName=DATN-DB`
+);
 
 //router
 app.use("/api", attributeRouter);
 app.use("/api", authRouter);
+app.use("/api/category", categoryRouter );
 
-export const viteNodeApp = app;
+
 export const viteNodeApp = app;
