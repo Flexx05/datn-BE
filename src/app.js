50f2898ec463059b34fc571b82a6d658d06bb6c3
Manh Linh <mlinhalone@gmail.com>
2025-05-28 11:52:18 +0700

    fix: app

--- src/app.js
+++ src/app.js
@@ -30,14 +30,15 @@ app.use(express.json());
 app.use(cookieParser());
 
 mongoose.connect(
-  `mongodb+srv://${process.env.DB_USERNAME}/${process.env.DB_URL}?retryWrites=true&w=majority&appName=DATN-DB`
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@datn-db.nx9ha3d.mongodb.net/${process.env.DB_URL}?retryWrites=true&w=majority&appName=DATN-DB`
 );
+console.log("Connected to MongoDB");
 
 //route
 app.use("/api", attributeRouter);
 app.use("/api", productRouter);
 app.use("/api", authRouter);
-app.use("/api", categoryRouter );
+app.use("/api", categoryRouter);
 app.use("/api", cartRouter);
 app.use("/api", brandRouter);
 app.use("/api", userRouter);
