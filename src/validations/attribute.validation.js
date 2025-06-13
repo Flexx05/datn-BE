6efa044c4782693b9d9dfc1e922e31001f2b88e6
Manh Linh <mlinhalone@gmail.com>
2025-06-13 20:01:35 +0700

    fix: increase maximum length for attribute name validation

--- src/validations/attribute.validation.js
+++ src/validations/attribute.validation.js
@@ -1,7 +1,7 @@
 import Joi from "joi";
 
 export const attributeSchema = Joi.object({
-  name: Joi.string().required().min(3).trim().max(20),
+  name: Joi.string().required().min(3).trim().max(50),
   values: Joi.array().items(Joi.string().min(3).trim().max(20)).required(),
   isColor: Joi.boolean(),
   isActive: Joi.boolean(),
