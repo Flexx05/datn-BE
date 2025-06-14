fdccf202e2bbba655c130a9edcae5d8061cc3efb
Manh Linh <mlinhalone@gmail.com>
2025-06-14 08:48:14 +0700

    fix: update minimum length for values in attribute validation schema

--- src/validations/attribute.validation.js
+++ src/validations/attribute.validation.js
@@ -2,7 +2,7 @@ import Joi from "joi";
 
 export const attributeSchema = Joi.object({
   name: Joi.string().required().min(3).trim().max(50),
-  values: Joi.array().items(Joi.string().min(3).trim().max(20)).required(),
+  values: Joi.array().items(Joi.string().min(1).trim().max(20)).required(),
   isColor: Joi.boolean(),
   isActive: Joi.boolean(),
 });
