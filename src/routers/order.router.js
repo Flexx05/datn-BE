cb1404db8595a4a8181a55fe9242fe06fd45e0de
Phongln <phonglnph51412@gmail.com>
2025-06-20 20:35:41 +0700

    fix: sửa cập nhật trạng thái đơn hàng

--- src/routers/order.router.js
+++ src/routers/order.router.js
@@ -15,9 +15,10 @@ const router = Router();
 router.post("/order", createOrder); // ai cũng đặt hàng được
 router.get("/order", verifyToken, isAdminOrStaff, getAllOrders); // chỉ admin hoặc nhân viên mới xem được tất cả đơn hàng
 router.get("/order/user", verifyToken, getOrderByUserId); // người dùng có thể xem tất cả đơn hàng của mình
-router.get("/order/:id", verifyToken, getOrderById); // người dùng có thể xem đơn hàng của mình, admin hoặc nhân viên có thể xem đơn hàng theo id
+router.get("/order/id/:id", verifyToken, getOrderById); // người dùng có thể xem đơn hàng của mình, admin hoặc nhân viên có thể xem đơn hàng theo id
 router.patch("/order/status/:id", verifyToken, isAdminOrStaff, updateOrderStatus);
 router.patch("/order/payment-status/:id", verifyToken, isAdmin, updatePaymentStatus); // chỉ admin mới có thể cập nhật trạng thái thanh toán của đơn hàng (dành cho COD và các đơn có khiếu nại), chưa làm: hệ thống sẽ tự động cập nhật trạng thái đơn hàng khi thanh toán online thành công hoặc khi hoàn tiền thành công
 router.patch("/order/cancel/:id", verifyToken, cancelOrder);  // người dùng có thể hủy đơn hàng của mình, admin hoặc nhân viên có thể hủy đơn hàng của người khác
 
 export default router;
+ 
